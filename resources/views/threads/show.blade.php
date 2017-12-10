ee791bdc1f572a18330a3925e10442be138480cb
Mirahmad Mirzajonov <kvadro48@mail.ru>
2017-12-10 16:57:30 +0200

    modified

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container"ii>
     <div class="row">
         <div class="col-md-8">
             <div class="panel panel-default">
