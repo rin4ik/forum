63e8b0deb69aa64b7b23bb03bf84cc2722803753
Mirahmad Mirzajonov <kvadro48@mail.ru>
2017-12-15 21:55:10 +0200

    some changes

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app') @section('content') @if(count($activities))
+@extends('layouts.app') @section('content')
 <div class="container">
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
@@ -6,28 +6,18 @@
         <h1> {{$profileUser->name}}
         </h1>
       </div>
-      @foreach($activities as $date => $activity)
+      @forelse($activities as $date => $activity)
       <h3 class="page-header">
         {{$date}}
       </h3>
       @foreach($activity as $activ) @if(view()->exists("profiles.activities.{$activ->type}")) @include("profiles.activities.{$activ->type}")
       @endif
       @endforeach
-      @endforeach {{-- {{$activity->links()}} --}}
-    </div>
-  </div>
-</div>
-@else
-<div class="container">
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-      <div class="page-header">
-        <h1> {{$profileUser->name}}
-        </h1>
-      </div>
+      @empty
       <h4>There is no activity for this user yet!</h4>
+      @endforelse {{-- {{$activity->links()}} --}}
     </div>
   </div>
 </div>
-@endif 
+
 @endsection
