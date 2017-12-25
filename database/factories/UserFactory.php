5f6f251093231cc43b547ebef30009c6587e6d2f
Mirahmad Mirzajonov <kvadro48@mail.ru>
2017-12-25 13:15:23 +0200

    44

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,11 @@ $factory->define(App\User::class, function (Faker $faker) {
     ];
 });
 $factory->define(App\Channel::class, function ($faker) {
-    $name=$faker->word;
-  
+    $name = $faker->word;
+
     return [
-       'name'=>$name,
-       'slug'=>$name
+       'name' => $name,
+       'slug' => $name
     ];
 });
 $factory->define(App\Thread::class, function ($faker) {
@@ -39,7 +39,7 @@ $factory->define(App\Thread::class, function ($faker) {
         'channel_id' => function () {
             return factory('App\Channel')->create()->id;
         },
-        
+
         'title' => $faker->sentence,
         'body' => $faker->paragraph
     ];
@@ -59,14 +59,23 @@ $factory->define(App\Reply::class, function ($faker) {
 });
 $factory->define(App\Favorite::class, function ($faker) {
     return [
-     
-
         'user_id' => function () {
             return factory('App\User')->create()->id;
         },
            'favorited_id' => function () {
-            return factory('App\Reply')->create()->id;
-        },
+               return factory('App\Reply')->create()->id;
+           },
         'favorited_type' => 'App\Reply'
+    ];
+});
+$factory->define(\Illuminate\Notifications\DatabaseNotification::class, function ($faker) {
+    return [
+            'id' => \Ramsey\Uuid\Uuid::uuid4()->toString(),
+            'type' => 'App\Notifications\ThreadWasUpdated',
+            'notifiable_id' => function () {
+                return auth()->id() ?: factory('App\User')->create()->id();
+            },
+            'notifiable_type' => 'App\User',
+            'data' => ['foo' => 'bar']
     ];
 });
--- tests/Feature/NotificationTest.php
+++ tests/Feature/NotificationTest.php
@@ -4,17 +4,23 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Notifications\DatabaseNotification;
 
 class NotificationTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->signIn();
+    }
 
     /**
      * @test
      */
     public function a_notification_is_prepared_when_a_subscribed_thread_recieves_a_new_reply_that_is_not_by_current_user()
     {
-        $this->signIn();
         $thread = create('App\Thread')->subscribe();
         $this->assertCount(0, auth()->user()->fresh()->notifications);
         $thread->addReply([
@@ -34,12 +40,7 @@ class NotificationTest extends TestCase
      */
     public function a_user_can_fetch_their_unread_notificaions()
     {
-        $this->signIn();
-        $thread = create('App\Thread')->subscribe();
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'Some reply here'
-           ]);
+        create(DatabaseNotification::class);
         $response = $this->getJson('/profiles/' . auth()->user()->name . '/notifications/')->json();
         $this->assertCount(1, $response);
     }
@@ -49,12 +50,8 @@ class NotificationTest extends TestCase
      */
     public function a_user_can_mark_a_notification_as_read()
     {
-        $this->signIn();
-        $thread = create('App\Thread')->subscribe();
-        $thread->addReply([
-            'user_id' => create('App\User')->id,
-            'body' => 'Some reply here'
-           ]);
+        create(DatabaseNotification::class);
+
         $this->assertCount(1, auth()->user()->unreadNotifications);
         $notificationId = auth()->user()->unreadNotifications->first()->id;
         $this->delete('/profiles/' . auth()->user()->name . "/notifications/{$notificationId}");
